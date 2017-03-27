6ccc9ed907b2d4d45cd13aa42cebc6008e3b9a77
lkn <244143562@qq.com>
2017-03-27 21:40:39 +0800

    制作商品详情页

--- src/components/goods.vue
+++ src/components/goods.vue
@@ -11,6 +11,41 @@
       </div>
       <div class="goods-collect"><i class="iconfont icon-weishoucang"></i>&nbsp;收藏</div>
     </div>
+    <div class="detail-block">
+      <div class="detail-header">
+        <span>更多细节图</span>
+      </div>
+      <div class="detail-image">
+        <v-image source="/static/images/QZ1.png" size="contain"></v-image>
+      </div>
+      <div class="detail-image">
+        <v-image source="/static/images/QZ1.png" size="contain"></v-image>
+      </div>
+    </div>
+    <div class="recommend-block">
+      <div class="recommend-header">
+        <span>精选搭配</span>
+      </div>
+      <div class="recommend-list">
+        <div class="item">
+          <div class="image-block">
+            <v-image source="/static/images/QZ1.png"></v-image>
+          </div>
+          <div class="name">111</div>
+          <div class="price">111</div>
+        </div>
+        <div class="item">
+          <div class="image-block"></div>
+          <div class="name"></div>
+          <div class="price"></div>
+        </div>
+        <div class="item">
+          <div class="image-block"></div>
+          <div class="name"></div>
+          <div class="price"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
